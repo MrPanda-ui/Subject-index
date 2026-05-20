80e8995a93c54bb9c1b41af09254c82cf1e6c608
Илья Петенко <ilyapetenko92@gmail.com>
2026-05-20 23:41:19 +0300

    Create new Form
    Создание новой формы основной

---
+++ Subject index/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Subject_index
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(519, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 43);
+            button1.TabIndex = 2;
+            button1.Text = "Добавить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Слово";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(70, 20);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(166, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(242, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Странницы";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(331, 20);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(182, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(16, 61);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(664, 256);
+            dataGridView1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(16, 366);
+            button2.Name = "button2";
+            button2.Size = new Size(157, 51);
+            button2.TabIndex = 6;
+            button2.Text = "Загрузить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(179, 366);
+            button3.Name = "button3";
+            button3.Size = new Size(157, 51);
+            button3.TabIndex = 7;
+            button3.Text = "Печать";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(342, 366);
+            button4.Name = "button4";
+            button4.Size = new Size(157, 51);
+            button4.TabIndex = 7;
+            button4.Text = "Сохранить для редактирования";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 326);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Найти слово: ";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(122, 323);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(289, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(505, 366);
+            button5.Name = "button5";
+            button5.Size = new Size(157, 51);
+            button5.TabIndex = 7;
+            button5.Text = "Обновить";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(342, 423);
+            button6.Name = "button6";
+            button6.Size = new Size(157, 51);
+            button6.TabIndex = 7;
+            button6.Text = "Сохранить для документа";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 704);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Пердметный указатель";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label3;
+        private TextBox textBox3;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ Subject index/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System.Drawing.Printing;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+namespace Subject_index
+{
+    public partial class Form1 : Form
+    {
+        private bool isSave = false;
+ 
+
+
+        Index index;
+        private PrintDocument printDocument = new PrintDocument();
+        private PrintPreviewDialog printPreviewDialog = new PrintPreviewDialog();
+        private string textToPrint = "";
+        private Timer timer;
+        public Form1()
+        {
+            isSave = false;
+            var timer = new Timer();
+            timer.Interval = 5 * 60 * 1000;
+            timer.Tick += Timer_Tick;
+            InitializeComponent();
+            SetupDataGridView();
+            index = new Index();
+            // Подписка на событие двойного клика
+            dataGridView1.CellDoubleClick += dataGridView1_CellDoubleClick;
+            printDocument.PrintPage += PrintDocument_PrintPage;
+            printPreviewDialog.Document = printDocument;
+            textBox3.TextChanged += textBoxSearch_TextChanged;
+            this.FormClosing += Form1_FormClosed;
+        }
+
+        private void Form1_FormClosed(object? sender, FormClosingEventArgs e)
+        {
+            if (isSave == false)
+            {
+                e.Cancel = true;
+                DialogResult result = MessageBox.Show("Данные не сохранены, хотите сохранить?", "Предупреждение", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                if (result == DialogResult.Yes)
+                {
+                    btnSave_Click(sender, e);
+                }
+                else if (result == DialogResult.No)
+                {
+                    e.Cancel = false;
+                }
+            }
+            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            isSave = false;
+        }
+        private void SearchWords(string searchText)
+        {
+            dataGridView1.Rows.Clear();
+
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                foreach (var entry in index.entries)
+                {
+                    dataGridView1.Rows.Add(entry.Word, string.Join(", ", entry.pages));
+                }
+                return;
+            }
+
+            foreach (var entry in index.entries)
+            {
+                if (entry.Word.ToLower().Contains(searchText.ToLower()))
+                {
+                    dataGridView1.Rows.Add(entry.Word, string.Join(", ", entry.pages));
+                }
+            }
+
+            if (dataGridView1.Rows.Count == 0)
+            {
+                dataGridView1.Rows.Add($"Ничего не найдено", "");
+            }
+        }
+
+        // ОБРАБОТЧИК ВВОДА В ПОИСК
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            SearchWords(textBox3.Text);
+        }
+        private void SetupDataGridView()
+        {
+            // Очистка (на всякий случай)
+            dataGridView1.Columns.Clear();
+
+            // Создание колонки "Слово"
+            DataGridViewTextBoxColumn wordColumn = new DataGridViewTextBoxColumn();
+            wordColumn.Name = "WordColumn";
+            wordColumn.HeaderText = "Слово";
+            wordColumn.Width = 150;
+
+            // Создание колонки "Страницы"
+            DataGridViewTextBoxColumn pagesColumn = new DataGridViewTextBoxColumn();
+            pagesColumn.Name = "PagesColumn";
+            pagesColumn.HeaderText = "Страницы";
+            pagesColumn.Width = 250;
+
+            // Добавление колонок в DataGridView
+            dataGridView1.Columns.Add(wordColumn);
+            dataGridView1.Columns.Add(pagesColumn);
+
+            // Запрещаем пользователю добавлять строки
+            dataGridView1.AllowUserToAddRows = false;
+
+            // Растягиваем последнюю колонку
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void AddWordInDVG()
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Введите слово");
+                return;
+            }
+
+            if (!int.TryParse(textBox2.Text, out int page))
+            {
+                MessageBox.Show("Введите номер страницы");
+                return;
+            }
+            index.Addword(textBox1.Text, page);
+            //обновление datagridview
+            RefreshDataGridView();
+
+            // Очищаем поле страницы, но слово оставляем (чтобы добавить ещё страниц этому же слову)
+            textBox2.Clear();
+            textBox2.Focus();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Текстовые файлы|*.txt";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Получаем имя, которое ввел пользователь
+                string userFileName = saveDialog.FileName;
+
+                // Добавляем суффикс перед расширением
+                // Например: "мой_указатель.txt" → "мой_указатель_data.txt"
+                string directory = Path.GetDirectoryName(userFileName);
+                string fileNameWithoutExt = Path.GetFileNameWithoutExtension(userFileName);
+                string extension = Path.GetExtension(userFileName);
+
+                string newFileName = Path.Combine(directory, fileNameWithoutExt + "_Редактирование" + extension);
+
+                index.SaveToFile(saveDialog.FileName);
+                isSave = true;
+                MessageBox.Show("Сохранено!");
+            }
+        }
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Текстовые файлы|*.txt";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Получаем имя, которое ввел пользователь
+                string userFileName = saveDialog.FileName;
+
+                // Добавляем суффикс перед расширением
+                // Например: "мой_указатель.txt" → "мой_указатель_data.txt"
+                string directory = Path.GetDirectoryName(userFileName);
+                string fileNameWithoutExt = Path.GetFileNameWithoutExtension(userFileName);
+                string extension = Path.GetExtension(userFileName);
+
+                string newFileName = Path.Combine(directory, fileNameWithoutExt + "_Печать" + extension);
+
+                index.SaveToFile(saveDialog.FileName);
+                MessageBox.Show("Сохранено!");
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog LoadDialog = new OpenFileDialog();
+            LoadDialog.Filter = "Текстовые файлы|*.txt";
+
+            if (LoadDialog.ShowDialog() == DialogResult.OK)
+            {
+                index.LoadFromFile(LoadDialog.FileName);
+                MessageBox.Show("Указатели загружены");
+                RefreshDataGridView();
+            }
+        }
+        private void RefreshDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+
+            foreach (var entry in index.entries)
+            {
+                string pagesString = string.Join(", ", entry.pages);
+
+                dataGridView1.Rows.Add(entry.Word, pagesString);
+            }
+        }
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Проверяем, что клик не по заголовку (e.RowIndex >= 0)
+            if (e.RowIndex >= 0)
+            {
+                // Получаем слово из первой колонки (индекс 0)
+                string selectedWord = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+                // Вставляем слово в TextBox
+                textBox1.Text = selectedWord;
+
+                // Очищаем TextBox для страницы (чтобы пользователь ввёл новую)
+                textBox2.Clear();
+
+                // Ставим курсор в поле для ввода страницы
+                textBox2.Focus();
+            }
+        }
+        private string PreparePrintText()
+        {
+            string result = "ПРЕДМЕТНЫЙ УКАЗАТЕЛЬ\n";
+            result += "==================\n\n";
+
+            // Сортируем по алфавиту
+            var sortedEntries = index.entries.OrderBy(e => e.Word);
+            char currentLetter = '\0';
+            foreach (var entry in sortedEntries)
+            {
+                if (!String.IsNullOrEmpty(entry.Word))
+                {
+                    char firstletter = char.ToUpper(entry.Word[0]);
+                    if(firstletter != currentLetter)
+                    {
+                        currentLetter = firstletter;
+                        result += " \n";
+                        result += $"{currentLetter}\n";
+                        result += $"{new string('=', 20)}";
+                    }
+
+                    string pages = string.Join(", ", entry.pages);
+                    result += $"{entry.Word}";
+
+                    // Добавляем точки для выравнивания
+                    int dotsCount = 30 - entry.Word.Length;
+                    if (dotsCount > 0)
+                            result += new string('.', dotsCount);
+                    else
+                            result += " ";
+
+                    result += $" {pages}\n";
+                }
+            }
+
+            result += "\n==================\n";
+            result += $"Всего слов: {index.entries.Count}\n";
+            result += $"Дата печати: {DateTime.Now:dd.MM.yyyy HH:mm}";
+
+            return result;
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Font printFont = new Font("Courier New", 10);
+            float linesPerPage = e.MarginBounds.Height / printFont.GetHeight(e.Graphics);
+            float yPos = e.MarginBounds.Top;
+            int count = 0;
+
+            string[] lines = textToPrint.Split('\n');
+
+            while (count < lines.Length && yPos + printFont.GetHeight(e.Graphics) < e.MarginBounds.Bottom)
+            {
+                e.Graphics.DrawString(lines[count], printFont, Brushes.Black,
+                                      e.MarginBounds.Left, yPos);
+                yPos += printFont.GetHeight(e.Graphics);
+                count++;
+            }
+
+            if (count < lines.Length)
+            {
+                e.HasMorePages = true;
+            }
+            else
+            {
+                e.HasMorePages = false;
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (index.entries.Count == 0)
+            {
+                MessageBox.Show("Указатель пуст. Нечего печатать.", "Предупреждение");
+                return;
+            }
+
+            textToPrint = PreparePrintText();
+            printPreviewDialog.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RefreshDataGridView();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddWordInDVG();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            btnSave_Click(sender, e);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            btnExport_Click(sender, e);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            btnLoad_Click(sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            btnPrint_Click(sender,e);
+        }
+    }
+}
